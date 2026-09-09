87bc9c708455e703f202ced629a15e0bd2e7bd36
singhpatel927-tech <singhpatel927@gmail.com>
2026-09-10 01:38:22 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.gaurav.quickdial"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.gaurav.quickdial"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions { jvmTarget = "17" }
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
