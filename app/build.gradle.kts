a720a205ca35d0f449354bdf770d6e00d9fdbe41
singhpatel927-tech <singhpatel927@gmail.com>
2026-09-11 15:53:39 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.gaurav.quickdial"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
